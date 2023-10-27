fc94bbbe33f2a2420075d24cb7203051a2484d24
bn-tw2020 <skaxodn97@gmail.com>
2023-10-27 22:01:21 +0900

    feat: 문자열계산기 테스트 코드 작성

--- src/main/kotlin/calculator/domain/Operand.kt
+++ src/main/kotlin/calculator/domain/Operand.kt
@@ -3,18 +3,22 @@ package calculator.domain
 @JvmInline
 value class Operand(private val value: Double) {
 
-    fun add(input: Operand) = Operand(this.value + input.value)
+    infix fun add(input: Operand) = Operand(this.value + input.value)
 
-    fun minus(input: Operand) = Operand(this.value - input.value)
+    infix fun minus(input: Operand) = Operand(this.value - input.value)
 
-    fun multiply(input: Operand) = Operand(this.value * input.value)
+    infix fun multiply(input: Operand) = Operand(this.value * input.value)
 
-    fun divide(input: Operand): Operand {
-        require(input.value != 0.0) { "0이라는 숫자로 나눌 수 없습니다. " }
+    infix fun divide(input: Operand): Operand {
+        require(input.value != 0.0) { DIVIDE_ERROR_MESSAGE }
         return Operand(this.value / input.value)
     }
 
     override fun toString(): String {
         return "$value"
+    }
+
+    companion object {
+        const val DIVIDE_ERROR_MESSAGE = "0이라는 숫자로 나눌 수 없습니다."
     }
 }
---
+++ src/test/kotlin/calculator/OperandTest.kt
@@ -0,0 +1,63 @@
+package calculator
+
+import calculator.domain.Operand
+import calculator.domain.Operand.Companion.DIVIDE_ERROR_MESSAGE
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class OperandTest : BehaviorSpec({
+
+    Given("덧셈 결과 테스트") {
+        When("피연산자가 1, 2가 주어졌을 때") {
+            val operand1 = Operand(1.0)
+            val operand2 = Operand(2.0)
+            Then("1 + 2는 3이어야 한다") {
+                operand1 add operand2 shouldBe Operand(3.0)
+            }
+        }
+    }
+
+    Given("뺄셈 결과 테스트") {
+        When("피연산자가 2, 1가 주어졌을 때") {
+            val operand1 = Operand(2.0)
+            val operand2 = Operand(1.0)
+            Then("2 - 1는 1이어야 한다") {
+                operand1 minus operand2 shouldBe Operand(1.0)
+            }
+        }
+    }
+
+    Given("곱셈 결과 테스트") {
+        When("피연산자가 2, 1가 주어졌을 때") {
+            val operand1 = Operand(2.0)
+            val operand2 = Operand(1.0)
+            Then("2 * 1는 1이어야 한다") {
+                operand1 multiply operand2 shouldBe Operand(2.0)
+            }
+        }
+    }
+
+    Given("나눗셈 결과 테스트") {
+        When("피연산자가 4, 2가 주어졌을 때") {
+            val operand1 = Operand(4.0)
+            val operand2 = Operand(2.0)
+            Then("4 / 2는 1이어야 한다") {
+                operand1 divide operand2 shouldBe Operand(2.0)
+            }
+        }
+    }
+
+    Given("나눗셈 0으로 나누는 경우") {
+        When("피연산자가 4, 0가 주어졌을 때") {
+            val operand1 = Operand(4.0)
+            val operand2 = Operand(0.0)
+            Then("4 * 0은 에러가 발생해야한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    operand1 divide operand2
+                }
+                exception.message shouldBe DIVIDE_ERROR_MESSAGE
+            }
+        }
+    }
+})
