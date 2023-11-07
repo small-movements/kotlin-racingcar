0f9def46e5f4b6359db43de05de4b6aa1d248d56
bn-tw2020 <skaxodn97@gmail.com>
2023-11-07 14:16:59 +0900

    feat: 문자열계산기 테스트 코드 피드백 반영

--- src/test/kotlin/calculator/OperandTest.kt
+++ src/test/kotlin/calculator/OperandTest.kt
@@ -9,9 +9,9 @@ import io.kotest.matchers.shouldBe
 class OperandTest : BehaviorSpec({
 
     Given("덧셈 결과 테스트") {
+        val operand1 = Operand(1.0)
+        val operand2 = Operand(2.0)
         When("피연산자가 1, 2가 주어졌을 때") {
-            val operand1 = Operand(1.0)
-            val operand2 = Operand(2.0)
             Then("1 + 2는 3이어야 한다") {
                 operand1 add operand2 shouldBe Operand(3.0)
             }
@@ -19,9 +19,9 @@ class OperandTest : BehaviorSpec({
     }
 
     Given("뺄셈 결과 테스트") {
+        val operand1 = Operand(2.0)
+        val operand2 = Operand(1.0)
         When("피연산자가 2, 1가 주어졌을 때") {
-            val operand1 = Operand(2.0)
-            val operand2 = Operand(1.0)
             Then("2 - 1는 1이어야 한다") {
                 operand1 minus operand2 shouldBe Operand(1.0)
             }
@@ -29,9 +29,9 @@ class OperandTest : BehaviorSpec({
     }
 
     Given("곱셈 결과 테스트") {
+        val operand1 = Operand(2.0)
+        val operand2 = Operand(1.0)
         When("피연산자가 2, 1가 주어졌을 때") {
-            val operand1 = Operand(2.0)
-            val operand2 = Operand(1.0)
             Then("2 * 1는 1이어야 한다") {
                 operand1 multiply operand2 shouldBe Operand(2.0)
             }
@@ -39,9 +39,9 @@ class OperandTest : BehaviorSpec({
     }
 
     Given("나눗셈 결과 테스트") {
+        val operand1 = Operand(4.0)
+        val operand2 = Operand(2.0)
         When("피연산자가 4, 2가 주어졌을 때") {
-            val operand1 = Operand(4.0)
-            val operand2 = Operand(2.0)
             Then("4 / 2는 1이어야 한다") {
                 operand1 divide operand2 shouldBe Operand(2.0)
             }
@@ -49,9 +49,9 @@ class OperandTest : BehaviorSpec({
     }
 
     Given("나눗셈 0으로 나누는 경우") {
+        val operand1 = Operand(4.0)
+        val operand2 = Operand(0.0)
         When("피연산자가 4, 0가 주어졌을 때") {
-            val operand1 = Operand(4.0)
-            val operand2 = Operand(0.0)
             Then("4 * 0은 에러가 발생해야한다.") {
                 val exception = shouldThrow<IllegalArgumentException> {
                     operand1 divide operand2
